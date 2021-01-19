f5ddbd33ef9b0a57fa4c3c77ce825ad49f24a545
retwish <retwish.xyz@gmail.com>
2021-01-19 06:36:44 +0200

    Add comments

--- database.py
+++ database.py
@@ -13,20 +13,20 @@ DATABASE_PATH = config.get('DATABASE', 'PATH')
 
 def log(func):
     def wrapper(cat_id, cur):
-        db_log.info(f'Try to get id:{cat_id}')
+        db_log.info(f'Try to get id:{cat_id}')  # Logging
         return func(cat_id, cur)
     return wrapper
 
 
 def add_cat_to_bd(cat_url, cat_id, cur):
     cur.execute("SELECT cat_id FROM cats")
-    if cat_id not in cur.fetchall():
-        cur.execute("INSERT INTO cats VALUES (?, ?)", (cat_url, cat_id))
+    if cat_id not in cur.fetchall():  # Try to find cat_id in db
+        cur.execute("INSERT INTO cats VALUES (?, ?)", (cat_url, cat_id))  # Push new record to db
 
 
 @log
 def find_cat_id(cat_id, cur):
-    cur.execute('SELECT cat_url FROM cats WHERE cat_id=?', (cat_id,))
+    cur.execute('SELECT cat_url FROM cats WHERE cat_id=?', (cat_id,))  # Find pic by id
     url = cur.fetchone()
     return url
 
